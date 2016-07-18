c96e63e2d9bdaebb766b15d886d8719dd109dc08
Dan Neciu <Dan Neciu>
2016-07-18 09:31:01 +0300

    verifying pull

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -121,7 +121,7 @@ input[type="radio"]{
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
       <div class="panel panel-default">
-        <div class="panel-heading">Add Pokemon Location</div>
+        <div class="panel-heading">Add Pokemon Locations</div>
 
         <div class="panel-body">
          <!-- Display Validation Errors -->
